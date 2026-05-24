970d3a6bdeb7c3551196069bdadc34081be93e05
Sankalpkombey <sankalpkombey125@gmail.com>
2026-05-24 21:33:47 +0530

    add cloudinary and multer middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+ cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+ const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // Upload the file to Cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully, remove it from the server
+        console.log("File uploaded successfully to Cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // Remove the file from the server as it failed to upload to Cloudinary
+        return null;
+    }
+ }  
